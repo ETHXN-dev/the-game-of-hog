bf81666279d7f18c64b4a5f989d1795b4fb43423
ETHXN <migethanu@gmail.com>
2026-08-04 08:52:46 +0100

    Implement select_dice, a helper function that will simplify the implementation of play

--- hog.py
+++ hog.py
@@ -71,6 +71,10 @@ def select_dice(score, opponent_score):
     True
     """
     "*** YOUR CODE HERE ***"
+    if (score + opponent_score) % 7 == 0:
+        return four_sided
+    else:
+        return six_sided
 
 
 def other(who):
